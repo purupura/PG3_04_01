291c4166eab86872151f2b663950c2073410ecd8
k023g0096@g.neec.ac.jp <k023g0096@g.neec.ac.jp>
2025-02-12 15:56:42 +0900

    a

--- pg3-04_01-master/main.cpp
+++ pg3-04_01-master/main.cpp
@@ -9,14 +9,38 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
-	// 生成
-	GameManager* gameManager = new GameManager();
+	Scene* scene = new Scene();
+	scene->Initialize();
 
-	// ゲーム実行
-	gameManager->Run();
+	// キー入力結果を受け取る箱
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
 
-	// 解放
-	delete gameManager;
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		/// ↓更新処理ここから
+		scene->Update();
+		/// ↑更新処理ここまで
+
+		/// ↓描画処理ここから
+		scene->Draw();
+		/// ↑描画処理ここまで
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
 
 	// ライブラリの終了
 	Novice::Finalize();
